9539babfc9ea0931200528b8e68dfa0d7cdf7b1c
2445968904 <44425297+2445968904@users.noreply.github.com>
2023-03-19 16:21:32 +0800

    somechange

--- Source/Slime/Character/main/SlimePlayerCameraManager.cpp
+++ Source/Slime/Character/main/SlimePlayerCameraManager.cpp
@@ -2,4 +2,31 @@
 
 
 #include "SlimePlayerCameraManager.h"
+#include "DrawDebugHelpers.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+//这些变量当作宏来调用
+const FName NAME_CameraBehavior(TEXT("CameraBehavior"));
+const FName NAME_CameraOffset_X(TEXT("CameraOffset_X"));
+const FName NAME_CameraOffset_Y(TEXT("CameraOffset_Y"));
+const FName NAME_CameraOffset_Z(TEXT("CameraOffset_Z"));
+const FName NAME_Override_Debug(TEXT("Override_Debug"));
+const FName NAME_PivotLagSpeed_X(TEXT("PivotLagSpeed_X"));
+const FName NAME_PivotLagSpeed_Y(TEXT("PivotLagSpeed_Y"));
+const FName NAME_PivotLagSpeed_Z(TEXT("PivotLagSpeed_Z"));
+const FName NAME_PivotOffset_X(TEXT("PivotOffset_X"));
+const FName NAME_PivotOffset_Y(TEXT("PivotOffset_Y"));
+const FName NAME_PivotOffset_Z(TEXT("PivotOffset_Z"));
+const FName NAME_RotationLagSpeed(TEXT("RotationLagSpeed"));
+const FName NAME_Weight_FirstPerson(TEXT("Weight_FirstPerson"));
+
+
+
+ASlime_Base::ASlime_Base()
+{
+	
+	
+}
+
 
