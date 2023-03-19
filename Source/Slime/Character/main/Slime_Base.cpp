4df3d5cc002ee2a2b5c3a4fabd9f783358245665
2445968904 <44425297+2445968904@users.noreply.github.com>
2023-03-19 13:20:53 +0800

    新建项目

---
+++ Source/Slime/Character/main/Slime_Base.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/main/Slime_Base.h"
+
+// Sets default values
+ASlime_Base::ASlime_Base()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASlime_Base::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASlime_Base::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASlime_Base::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
