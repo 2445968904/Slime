0b3c66cb7b13c848adfa9d9acece8dc18fd69850
2445968904 <44425297+2445968904@users.noreply.github.com>
2023-03-19 14:01:12 +0800

    .ideaignore

--- Source/Slime/Character/main/Slime_Base.cpp
+++ Source/Slime/Character/main/Slime_Base.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Character/main/Slime_Base.h"
+#include "Slime_Base.h"
 
 // Sets default values
 ASlime_Base::ASlime_Base()
